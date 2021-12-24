2a4c2ff6aae6df73214f7186a9904596bea3ef3a
[3기-2반] 김현주 <nr25@naver.com>
2021-12-24 14:17:52 +0000

    Add new file

---
+++ back/load_data_deliver.py
@@ -0,0 +1,45 @@
+import csv
+from random import randint
+from datetime import date, datetime, timedelta, timezone
+import pymysql
+
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+db_connection = pymysql.connect(
+    user    = "root",
+    passwd  = "",
+    host    = "localhost",
+    port    = ,
+    db      = 'corona_web',
+    charset = 'utf8mb4'
+)
+
+
+con = db_connection
+cur = con.cursor(pymysql.cursors.DictCursor)
+# cur.execute("CREATE TABLE book IF NOT EXISTS (id integer primary key auto_increment, book_name varchar(50), publisher varchar(30), author varchar(30), publication_date DATETIME, pages integer, isbn bigint(20), description text, link varchar(256));")
+
+# csv 파일 읽기
+# 인코딩 문제시 encoding = ''에 utf-8 > euc-kr > cp949 순서로 해볼것
+with open('./models/seoul_deliver_count.csv', encoding='utf-8-sig') as data :
+    records = csv.DictReader(data)
+    result = []
+    # pdate = datetime.strptime(c['date'], '%Y-%m-%d').date()
+    # result = [ c['id'], c['date'], c['gu'], c['patient_count'] for c in records]
+    for c in records:
+      # print(records)
+      result.append([c['date'], c['gu'], c['dong'],c['deliver_count']])
+
+
+print(result[0])
+print('---')
+# print(*result[0])
+print(result[0][0])
+
+# csv 파일 MysQL에 삽입
+cur.executemany("insert into deliver(date, gu, dong, deliver_count) values(%s, %s,%s, %s)", result)
+
+
+con.commit()
+con.close()
