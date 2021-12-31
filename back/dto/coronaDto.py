5bf3e21100bafe42f8d4637c8fd79504c85054a3
rmsqor103 <rmsqor103@naver.com>
2021-12-31 14:52:02 +0900

    merge branch

--- back/dto/coronaDto.py
+++ back/dto/coronaDto.py
@@ -1,17 +1,32 @@
 from datetime import date
-from flask_restx import  Namespace,fields
+from flask_restx import Namespace, fields
+
 
 class DateFormat(fields.Raw):
     def format(self, value):
         return date.strftime(value, "%Y-%m-%d")
 
-# ns = api.namespace("data", description="코로나 발생 건수 api")
 
-class CoronaDto:
-    api = Namespace("corona_cnt", description="코로나 발생 건수 api")
-    pamo = api.model('pamo', {
-      'id': fields.Integer,
-      'gu': fields.String,
-      'date': DateFormat(),
-      'patient_count': fields.Integer
-    })
+class CovDto:
+    api = Namespace("cov", description="코로나 발생 건수 api")
+    patient_model = api.model(
+        "patient_model",
+        {
+            "id": fields.Integer(readonly=True, description='아이디'),
+            "gu": fields.String(readonly=True, description='지역 (구)'),
+            "date": DateFormat(),
+            "patient_count": fields.Integer,
+        },
+    )
+
+    delivery_model = api.model(
+        "delivery_model",
+        {
+            "id": fields.Integer,
+            "gu": fields.String,
+            "dong": fields.String,
+            "date": DateFormat(),
+            "deliver_count": fields.Integer,
+        },
+    )
+
